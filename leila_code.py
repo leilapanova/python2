87225cf562a6e4141ef3d4e1b792210b567466bb
leilapanova <144453728+leilapanova@users.noreply.github.com>
2023-10-05 14:21:53 +0200

    Add files via upload

---
+++ leila_code.py
@@ -0,0 +1,85 @@
+
+#task 1
+# a = input()
+# print(a)
+# print(a[2])
+# print(a[-2])
+# print(a[5])
+# print(a[0::2])
+# print(a[1::2])
+# print(a[::-1])
+# a = a[::-1]
+# print(a[::2])
+# print(len(a))
+#task 2
+# a = input()
+# print(a.count(' ')+1)
+#task 3
+# a = input()
+# b = len(a) // 2
+# print()
+#task 4
+# a = input()
+# print(a.find('f'))
+# print(a.rfind('f'))
+#task 5
+# a = input()
+# a = a[:a. find('h')] + a[a.rfind('h')]
+# print(a)
+#task 6
+# a = input()
+# print(a.replace('1' , 'one'))
+#task 7
+# a = input()
+# print(a.replace('@' , ''))
+#task 8
+#  a = input()
+#  print(a.replace('h' , 'H', a.count('h') -1)
+# print(a.replace('h', 'H', 1))
+#task 9
+# a = int(input())
+# b = int(input())
+# if a < b:
+#     print(a)
+# else:
+#     print(b)
+#task 10
+# x = int(input())
+# if x > 0:
+#     print(1)
+# elif x < 0:
+#     print(-1)
+# else:
+#     print(0)
+#task 11
+# x1 = int(input())
+# x2 = int(input())
+# y1= int(input())
+# y2 = int(input())
+#
+# if 1 <= x1 <= 8 and 1<= y1 <= 8 and 1 <= x2 <= 8 and 1 <= y2 <= 8:
+#     if (x1 + y1) % 2 == 0 and (x2+y2) % 2 == 0:
+#         print('yes')
+#     elif (x1 + y1) % 2 == 1 and (x2 + y2) % 2 == 1:
+#         print('yes')
+#     else:
+#         print('no')
+# else:
+#     print('error')
+#task 12
+# year = int(input())
+# if year%4 == 0 and year%100!= 0 and year%400 == 0:
+#     print('yes')
+# else:
+#     print('no')
+#task 13
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# if a == b == c:
+#     print(3)
+# elif a == b or a == c or b ==c:
+#     print(2)
+# else:
+#     print(0)
+    
